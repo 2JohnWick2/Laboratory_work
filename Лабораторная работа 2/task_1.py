41e84cebf189fd42f3b909eda853eb876de7d329
John_Wick <145047601+2JohnWick2@users.noreply.github.com>
2024-10-27 19:35:52 +0300

    Update task_1.py

--- Лабораторная работа 2/task_1.py
+++ Лабораторная работа 2/task_1.py
@@ -6,4 +6,6 @@ money_capital = 0
 for month in range(months):
     money_capital += spend - salary
     spend *= (1 + increase)
+money_capital = ((money_capital + 99) // 100) * 100
 print(f"Подушка безопасности, чтобы протянуть {months} месяцев без долгов: {round(money_capital)}")
+
